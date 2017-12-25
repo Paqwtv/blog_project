bf867264e064f2b03d71dce29e2dfee945880a2c
Paqwtv <paqwtv@gmail.com>
2017-12-25 13:53:15 +0200

    fix for validators

--- app/validators/pool_validator.rb
+++ app/validators/pool_validator.rb
@@ -1,14 +1,21 @@
+# custom validates for Users, Posts and Categories
 class PoolValidator < ActiveModel::EachValidator
   def validate_each(record, attr, value)
-    p record
+    words = value.split(' ')
+    if record.class == User
+      words.each do |word|
+        record.errors.add(attr, 'Ваше имя должно начинаться с заглавной буквы') if
+        word =~ /\A[[:lower:]]/
+      end
+    end
+
     record.errors.add(attr, 'Название должно начинаться с заглавной буквы') if
     value =~ /\A[[:lower:]]/
-    
-    post_name = value.split(' ')
-    record.errors.add(attr, 'Название должно состоять как минимум из двух слов') if
-    post_name.size < 2
 
-    post_name.each do |word|
+    record.errors.add(attr, 'Название должно состоять как минимум из двух слов') if
+    words.size < 2
+
+    words.each do |word|
       record.errors.add(attr, 'Каждое слово должно состоять из двух символов') if
       word.size < 2
     end
